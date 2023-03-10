cac65dc4624b7d556a61bf53ab18656d739f5d32
KungFuTurtle <m.rfischer5235@gmail.com>
2023-03-09 21:49:45 -0600

    working on 2 users setup

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,64 +1,72 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
+const { User, Volunteer, Charity } = require('../models');
 const { signToken } = require('../utils/auth');
-
 const resolvers = {
-Query:{
-     getSingleUser: async function(parent,args,context ) {
-        const foundUser = await User.findOne({
-          $or: [{ _id: context.user._id }, { username: args.username }],
-        });
-    
-        if (!foundUser) {
-            throw new AuthenticationError('You need to be logged in!');
-        }
-    
-        return foundUser;
-      },
-      me: async function(parent,args,context ) {
-        const foundUser = await User.findOne({
-            _id: context.user._id ,
-        });
-    
-        if (!foundUser) {
-            throw new AuthenticationError('You need to be logged in!');
-        }
-    
-        return foundUser;
-      },
-
-},
-Mutation:{ 
-     createUser:async function(parent, args ) {
-        const user = await User.create(args);
-    
-        if (!user) {
-            throw new AuthenticationError('You need to be logged in!');
-        }
-        const token = signToken(user);
-        return {token, user} ;
-      },
-      // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
-      // {body} is destructured req.body
-       login:async function(parent, args) {
-        const user = await User.findOne({ $or: [{ username: args.username }, { email: args.email }] });
-        if (!user) {
-            throw new AuthenticationError('You need to be logged in!');
-        }
-    
-        const correctPw = await user.isCorrectPassword(args.password);
-    
-        if (!correctPw) {
-          
-        }
-        const token = signToken(user);
-        return{ token, user };
-      },
-      
-    
-      
+  User: {
+    __resolveType(user, context, info) {
+      if (user.usertype === 'VOLUNTEER') {
+        return 'Volunteer';
+      } else if (user.usertype === 'CHARITY') {
+        return 'Charity';
+      } else {
+        return new AuthenticationError('Must have User Type');
+      }
     }
-    
+  },
 
-}
+  Query: {
+    getSingleUser: async function(parent, args, context) {
+      const foundUser = await User.findOne({
+        $or: [{ _id: context.user._id }, { username: args.username }],
+      });
+
+      if (!foundUser) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+
+      return foundUser;
+    },
+
+    me: async function(parent, args, context) {
+      const foundUser = await User.findOne({
+        _id: context.user._id,
+      });
+
+      if (!foundUser) {
+        throw new AuthenticationError('You need to be logged in!');
+      }
+
+      return foundUser;
+    },
+  },
+
+  Mutation: {
+    createVolunteer: async function(parent, args) {
+      const { username, email, password } = args;
+      // Hash the password before storing it
+      
+      const newVolunteer = await Volunteer.create({ username, email, password,skills, usertype: 'VOLUNTEER' });
+
+      if (!newVolunteer) {
+        throw new AuthenticationError('Error creating volunteer');
+      }
+
+      const token = signToken(newVolunteer);
+      return { token, user: newVolunteer };
+    },
+
+    createCharity: async function(parent, args) {
+      const { username, email, password,  } = args;
+      
+      const newChartiy = await Charity.create({ username, email, password, charityName, usertype: 'CHARITY' });
+
+      if (!newChartiy) {
+        throw new AuthenticationError('Error creating volunteer');
+      }
+
+      const token = signToken(newChartiy);
+      return { token, user: newChartiy };
+    },
+  },
+} 
 module.exports=resolvers
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,50 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type Auth{
-    token:String
-    user:User
-}
-type User{
-    _id:ID
-    username:String!
-    email:String!
-}
 
-
-type Query{
-    getSingleUser(username:String):User
-    me:User
-}
-type Mutation{
-    createUser(username:String!,email:String!,password:String!):Auth
-    login(email: String!, password: String!,):Auth
+enum UserType {
+    VOLUNTEER
+    CHARITY
   }
-`
+  
+  interface User {
+    _id: ID!
+    username: String!
+    email: String!
+    usertype: UserType!
+  }
+  
+  type Volunteer implements User {
+    _id: ID!
+    username: String!
+    email: String!
+    usertype: UserType!
+    password: String!
+    skills: String!
+  }
+  
+  type Charity implements User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    usertype: UserType!
+    charityName: String!
+  }
+  
+  type Auth {
+    token: String
+    user: User
+  }
+  
+  type Query {
+    getSingleUser(username: String!): User
+    me: User
+  }
+  
+  type Mutation {
+    createVolunteer(username: String!, email: String!, password: String! skill: String!): Auth
+    createCharity(username: String!, email: String!, password: String!, charityName: String!): Auth
+    login(email: String!, password: String!): Auth
+  }`
 module.exports=typeDefs
