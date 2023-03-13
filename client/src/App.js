ac4be48b8e53aad8cf3da280b0595e01c0ae6fad
Grace <112779767+grachimus@users.noreply.github.com>
2023-03-12 22:38:39 -0500

    finished event cards and started homepage style

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 // ------------------- ORIGINAL CODE ---------------------
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Home";
 
