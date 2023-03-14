c130c19ea78c3d6055b47173472d6f36d8d48bcb
Jior <JamesIoriattiII2023@u.northwestern.edu>
2023-03-14 14:08:39 -0500

    Migrated volunteers/signup code to VolunteerSignup route, removed uneeded comments

--- client/src/pages/volunteers/signup.js
+++ client/src/pages/volunteers/signup.js
@@ -3,7 +3,6 @@ import googleOneTap from 'google-one-tap'
 import { useStateContext,useDispatchContext } from "../../utils/GlobalState";
 import ACTIONS from '../../utils/actions'
 import { ADD_GOOGLE_VOLUNTEER, ADD_VOLUNTEER } from "../../utils/mutations";
-import { QUERY_GOOGLE_VOLUNTEER } from "../../utils/queries";
 import { useMutation, useQuery } from "@apollo/client";
 import Auth from "../../utils/auth";
 import { useState } from "react";
@@ -119,58 +118,63 @@ export default function GoogleSignUp() {
        
         
     return (
-        <div className="container my-1">
-      <h2>Sign Up</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="username">Username</label>
-          <input
-            placeholder="Amazing person"
-            name="username"
-            type="username"
-            id="username"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="email@domain.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="skills">Skills:</label>
-          <input
-            placeholder="Organizing, outgoing,"
-            name="skills"
-            type="skills"
-            id="skills"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
+      <div className="container my-1">
+        <h1>Sign Up</h1>
+        <form onSubmit={handleFormSubmit}>
+          <div className="flex-row space-between my-2">
+            <label htmlFor="username">Username</label>
+            <input
+              placeholder="Amazing person"
+              name="username"
+              type="username"
+              id="username"
+              onChange={handleChange}
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
           </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-    )
+          <div className="flex-row space-between my-2">
+            <label htmlFor="email">Email:</label>
+            <input
+              placeholder="email@domain.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
+          </div>
+          <div className="flex-row space-between my-2">
+            <label htmlFor="pwd">Password:</label>
+            <input
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
+          </div>
+          <div className="flex-row space-between my-2">
+            <label htmlFor="skills">Skills:</label>
+            <input
+              placeholder="Organizing, outgoing,"
+              name="skills"
+              type="skills"
+              id="skills"
+              onChange={handleChange}
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
+          </div>
+          {error ? (
+            <div>
+              <p className="error-text">The provided credentials are incorrect</p>
+            </div>
+          ) : null}
+          <div className="flex-row flex-end">
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+          </div>
+        </form>
+  
+      </div>
+    );
 }
