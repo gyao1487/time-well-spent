9a1654da4c80b6c8759cf99d37172e8403049b49
Jior <JamesIoriattiII2023@u.northwestern.edu>
2023-03-15 15:22:53 -0500

    Added google volunteer login resolver/functionality with google one-tap login capability.

--- client/src/pages/LoginVolunteer.js
+++ client/src/pages/LoginVolunteer.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import googleOneTap from 'google-one-tap'
 import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
-import { LOGIN_VOLUNTEER } from '../utils/mutations';
+import { LOGIN_GOOGLE_VOLUNTEER, LOGIN_VOLUNTEER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 function LoginVolunteer(props) {
   const [uservformState, setFormState] = useState({ username: '', password: '' });
   const [loginAsVolunteer, { error }] = useMutation(LOGIN_VOLUNTEER);
-
+  const [loginAsGoogleVolunteer, {err} ] = useMutation(LOGIN_GOOGLE_VOLUNTEER);
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -28,6 +29,55 @@ function LoginVolunteer(props) {
       [name]: value,
     });
   };
+  useEffect(()=>{
+    if(Auth.loggedIn()){
+      return;
+    }
+    const clientID = process.env.REACT_APP_GOOGLE_CLIENT_ID
+    const options = {
+        client_id: clientID, 
+        auto_select: false, 
+        cancel_on_tap_outside: false, 
+        context: 'signin',
+    };
+    function decodeJwtResponse(token) {
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload);
+      }
+
+    googleOneTap(options, async (response) => {
+        const res = await fetch('/api/google/login',{
+            method: 'POST',
+            body: JSON.stringify({
+                token: response.credential
+            }),
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+        });
+        if(res.ok){
+            const userData = await res.json();
+            console.log(userData);
+            const { name: username, email, picture, sub, jti } = userData;
+            const {data, error} = await loginAsGoogleVolunteer({
+              variables: {
+                email: email,
+                jti: jti,
+              }
+            })
+            const responsePayload = decodeJwtResponse(response.credential);
+            console.log(responsePayload);
+            localStorage.setItem('ID', JSON.stringify(data.loginAsGoogleVolunteer.googlev._id));
+            Auth.login(data.loginAsGoogleVolunteer.token);
+            
+        }
+    });
+ },[])
 
   return (
     <div className="container my-1">
