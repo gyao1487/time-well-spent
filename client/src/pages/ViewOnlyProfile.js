ae5d81bc9261a0db67537e5c67abb8b8ecadd90d
Grace <112779767+grachimus@users.noreply.github.com>
2023-03-18 18:22:41 -0500

    added loading screen

--- client/src/pages/ViewOnlyProfile.js
+++ client/src/pages/ViewOnlyProfile.js
@@ -1,21 +1,26 @@
-import styles from '../styles/Profile.module.css'
-import React from "react";import { QUERY_CHARITY, QUERY_CHARITY_BY_USERNAME} from '../utils/queries';
-import { useQuery } from '@apollo/client';
-import { useParams } from 'react-router-dom';
-import { gql } from '@apollo/client';
-import { Link } from 'react-router-dom';
+import styles from "../styles/Profile.module.css";
+import React from "react";
+import { QUERY_CHARITY, QUERY_CHARITY_BY_USERNAME } from "../utils/queries";
+import { useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+import { gql } from "@apollo/client";
+import { Link } from "react-router-dom";
+import Loading from "../components/Loading";
 
-function ViewOnlyProfile(){
-  const {username} = useParams();
-    console.log(username)
-    const {loading, error, data} = useQuery(QUERY_CHARITY_BY_USERNAME, {
-      variables: {username: username}
-    });
-    const charity = data?.charity
-  
+function ViewOnlyProfile() {
+  const { username } = useParams();
+  console.log(username);
+  const { loading, error, data } = useQuery(QUERY_CHARITY_BY_USERNAME, {
+    variables: { username: username },
+  });
+  const charity = data?.charity;
+
   //Make better loading screen/
-    if (loading) return <p>Loading</p>
-    if (error) return <p>{error.message}</p>
+  if (loading)
+    return (
+  <Loading />
+    );
+  if (error) return <p>{error.message}</p>;
 
   return (
     <div>
@@ -33,24 +38,25 @@ function ViewOnlyProfile(){
                     ></img>
                   </div>
                 </div>
-{/* ----------------------- ICONS ------------------------- */}
+                {/* ----------------------- ICONS ------------------------- */}
                 <div className="w-full px-4 justify-center mt-20">
                   <div className="flex justify-center py-4 lg:pt-4 pt-8">
                     <div className="mr-4 p-3 text-center items-center ">
                       <div className="flex justify-center space-x-2">
                         <div>
                           <a
-                          href ={`https://${charity.websiteURL}`}
-                          target='blank'
-                          rel="noopener"
+                            href={`https://${charity.websiteURL}`}
+                            target="blank"
+                            rel="noopener"
                           >
-                          <button
-                            type="button"
-                            data-te-ripple-init
-                            data-te-ripple-color="light"
-                            className="inline-block rounded-full bg-primary p-2 uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-                          >
-                            <svg
+                            <button
+                              type="button"
+                              data-te-ripple-init
+                              data-te-ripple-color="light"
+                              className="inline-flex items-center justify-center w-8 h-8 mr-2  rounded-full bg-primary p-2 uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                            >
+                              <i className="fa-solid fa-globe"></i>
+                              {/* <svg
                               xmlns="http://www.w3.org/2000/svg"
                               viewBox="0 0 24 24"
                               fill="currentColor"
@@ -61,8 +67,8 @@ function ViewOnlyProfile(){
                                 d="M19.5 21a3 3 0 003-3V9a3 3 0 00-3-3h-5.379a.75.75 0 01-.53-.22L11.47 3.66A2.25 2.25 0 009.879 3H4.5a3 3 0 00-3 3v12a3 3 0 003 3h15zm-6.75-10.5a.75.75 0 00-1.5 0v4.19l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V10.5z"
                                 clipRule="evenodd"
                               />
-                            </svg>
-                          </button>
+                            </svg> */}
+                            </button>
                           </a>
                         </div>
                       </div>
@@ -70,8 +76,7 @@ function ViewOnlyProfile(){
                       <span className="text-sm text-blueGray-400">Website</span>
                     </div>
 
-
-          {/* ----------------------------------------------------------- */}
+                    {/* ----------------------------------------------------------- */}
                     <div className="mr-4 p-3 text-center">
                       <div className="flex justify-center space-x-2">
                         <div>
@@ -79,9 +84,10 @@ function ViewOnlyProfile(){
                             type="button"
                             data-te-ripple-init
                             data-te-ripple-color="light"
-                            className="inline-block rounded-full bg-primary p-2 uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                            className="inline-flex items-center justify-center w-8 h-8 mr-2  rounded-full bg-primary p-2 uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
                           >
-                            <svg
+                            <i className="fa-brands fa-twitter"></i>
+                            {/* <svg
                               xmlns="http://www.w3.org/2000/svg"
                               viewBox="0 0 24 24"
                               fill="currentColor"
@@ -92,12 +98,12 @@ function ViewOnlyProfile(){
                                 d="M19.5 21a3 3 0 003-3V9a3 3 0 00-3-3h-5.379a.75.75 0 01-.53-.22L11.47 3.66A2.25 2.25 0 009.879 3H4.5a3 3 0 00-3 3v12a3 3 0 003 3h15zm-6.75-10.5a.75.75 0 00-1.5 0v4.19l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V10.5z"
                                 clipRule="evenodd"
                               />
-                            </svg>
+                            </svg> */}
                           </button>
                         </div>
                       </div>
 
-                      <span className="text-sm text-blueGray-400">Website</span>
+                      <span className="text-sm text-blueGray-400">Twitter</span>
                     </div>
                     <div className="lg:mr-4 p-3 text-center">
                       <div className="flex justify-center space-x-2">
@@ -106,9 +112,10 @@ function ViewOnlyProfile(){
                             type="button"
                             data-te-ripple-init
                             data-te-ripple-color="light"
-                            className="inline-block rounded-full bg-primary p-2 uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                            className="inline-flex items-center justify-center w-8 h-8 mr-2  rounded-full bg-primary p-2 uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
                           >
-                            <svg
+                            <i className="fa-brands fa-instagram"></i>
+                            {/* <svg
                               xmlns="http://www.w3.org/2000/svg"
                               viewBox="0 0 24 24"
                               fill="currentColor"
@@ -119,11 +126,13 @@ function ViewOnlyProfile(){
                                 d="M19.5 21a3 3 0 003-3V9a3 3 0 00-3-3h-5.379a.75.75 0 01-.53-.22L11.47 3.66A2.25 2.25 0 009.879 3H4.5a3 3 0 00-3 3v12a3 3 0 003 3h15zm-6.75-10.5a.75.75 0 00-1.5 0v4.19l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V10.5z"
                                 clipRule="evenodd"
                               />
-                            </svg>
+                            </svg> */}
                           </button>
                         </div>
                       </div>
-                      <span className="text-sm text-blueGray-400">Comments</span>
+                      <span className="text-sm text-blueGray-400">
+                        Instagram
+                      </span>
                     </div>
                   </div>
                 </div>
@@ -142,14 +151,12 @@ function ViewOnlyProfile(){
                 </div>
                 <div className="mb-2 text-blueGray-600">
                   <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>
-                  Website
                 </div>
               </div>
               <div className="mt-10 py-5 border-t border-blueGray-200 text-center">
                 <div className="flex flex-wrap justify-center flex-col items-center">
                   <div className="w-full lg:w-9/12 px-4">
-          
-                <p>Description</p>
+                    <p>{charity.description}</p>
                   </div>
                 </div>
               </div>
@@ -159,6 +166,6 @@ function ViewOnlyProfile(){
       </section>
     </div>
   );
-};
+}
 
 export default ViewOnlyProfile;
