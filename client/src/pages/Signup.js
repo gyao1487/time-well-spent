9a881aa30b5dba4d22b4023022caf64e2f080df4
Grace <112779767+grachimus@users.noreply.github.com>
2023-03-17 23:42:03 -0500

    added -OR- to Sign up page and closed gap

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -5,73 +5,58 @@
 //Background image
 // Buttons for two options as well as images/icons?
 
-import React from 'react';
-
+import React from "react";
 
 const Signup = () => {
-  return ( 
+  return (
     <div>
-
-
-<div className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-10 ">
           <div className="text-center">
-            
             <h1 className="text-4xl font-bold tracking-tight dark:text-white sm:text-6xl">
               How can we help?
             </h1>
-
           </div>
         </div>
-        <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-          <svg
-            className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-            viewBox="0 0 1155 678"
-          >
-            <path
-              fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-              fillOpacity=".3"
-              d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-            />
-            <defs>
-              <linearGradient
-                id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-                x1="1155.49"
-                x2="-78.208"
-                y1=".177"
-                y2="474.645"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor="#9089FC" />
-                <stop offset="1" stopColor="#FF80B5" />
-              </linearGradient>
-            </defs>
-          </svg>
-        </div>  
+        
+        <div className="flex items-center justify-center flex-col w-full lg:flex-row">
 
-        <div className="flex flex-wrap items-center justify-center" id="button-elements">
-          <button
+        <button
             type="button"
-            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-4xl px-6 py-8 text-center m-2"
+            className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-small rounded-lg text-4xl px-6 py-8 text-center m-2"
             data-te-ripple-init
             data-te-ripple-color="light"
-            onClick ={ ()=> document.location.replace('/VolunteerSignup') }>
+            onClick={() => document.location.replace("/VolunteerSignup")}
+          >
             I want to help
           </button>
+
+          <div className="divider lg:divider-horizontal">OR</div>
+          
           <button
             type="button"
-            className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-4xl px-6 py-8 text-center m-2"
+            className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-small rounded-lg text-4xl px-6 py-8 text-center m-2"
             data-te-ripple-init
             data-te-ripple-color="light"
-            onClick ={ ()=> document.location.replace('/CharitySignup') }>
+            onClick={() => document.location.replace("/CharitySignup")}
+          >
             I'm looking for volunteers
           </button>
         </div>
+
+        
+       
+
+        <div
+          className="flex flex-wrap items-center justify-center"
+          id="button-elements"
+        >
+     
+        
+        </div>
+      </div>
     </div>
-</div>
-
-
-  )
+  );
 };
 
 export default Signup;
