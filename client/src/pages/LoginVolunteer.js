2b075018a4e62243fed313ad88f7780381815654
Grace <112779767+grachimus@users.noreply.github.com>
2023-03-18 00:15:26 -0500

    added volunteer signin styling. Working on form validation

--- client/src/pages/LoginVolunteer.js
+++ client/src/pages/LoginVolunteer.js
@@ -4,16 +4,30 @@ import { useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { LOGIN_GOOGLE_VOLUNTEER, LOGIN_VOLUNTEER } from "../utils/mutations";
 import Auth from "../utils/auth";
+import { LockClosedIcon } from "@heroicons/react/20/solid";
 
 function LoginVolunteer(props) {
   const [uservformState, setFormState] = useState({
     username: "",
     password: "",
   });
+
+  //Error messages:
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [displayUsernameError, setDisplayUsernameError] = useState(false);
+  const [displayPasswordError, setDisplayPasswordError] = useState(false);
+
   const [loginAsVolunteer, { error }] = useMutation(LOGIN_VOLUNTEER);
   const [loginAsGoogleVolunteer, { err }] = useMutation(LOGIN_GOOGLE_VOLUNTEER);
+
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    // Reset error messages and display status
+    setUsernameError("");
+    setDisplayUsernameError(false);
+    setPasswordError("");
+    setDisplayPasswordError(false);
     try {
       const mutationResponse = await loginAsVolunteer({
         variables: {
@@ -22,10 +36,18 @@ function LoginVolunteer(props) {
         },
       });
       const token = mutationResponse.data.loginAsVolunteer.token;
-      console.log(token)
+      console.log(token);
       Auth.login(token);
     } catch (e) {
       console.log(e);
+      // Update error messages and display status based on the error
+      if (e.message.includes("Username")) {
+        setUsernameError(e.message);
+        setDisplayUsernameError(true);
+      } else if (e.message.includes("Password")) {
+        setPasswordError(e.message);
+        setDisplayPasswordError(true);
+      }
     }
   };
 
@@ -35,7 +57,38 @@ function LoginVolunteer(props) {
       ...uservformState,
       [name]: value,
     });
+
+    // Reset error messages and display status when the user changes the input fields
+    if (name === "username") {
+      setUsernameError("");
+      setDisplayUsernameError(false);
+    } else if (name === "password") {
+      setPasswordError("");
+      setDisplayPasswordError(false);
+    }
   };
+
+  // Render error popups based on the display status and error messages
+  const renderUsernameError = () => {
+    if (displayUsernameError) {
+      return (
+        <div className="absolute top-0 mt-2 text-red-600 text-sm">
+          {usernameError}
+        </div>
+      );
+    }
+  };
+
+  const renderPasswordError = () => {
+    if (displayPasswordError) {
+      return (
+        <div className="absolute top-0 mt-2 text-red-600 text-sm">
+          {passwordError}
+        </div>
+      );
+    }
+  };
+
   useEffect(() => {
     if (Auth.loggedIn()) {
       return;
@@ -92,7 +145,112 @@ function LoginVolunteer(props) {
   }, []);
 
   return (
-    <div classNameName="container my-1">
+    <>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              If you're a charity,{" "}
+              <Link
+                to="/LoginCharity"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                click here to sign in instead!
+              </Link>
+            </p>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={handleFormSubmit}>
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="username" className="sr-only">
+                  Username
+                </label>
+                <input
+                  placeholder="Username"
+                  name="username"
+                  type="text"
+                  id="username"
+                  onChange={handleChange}
+                  autoComplete="username"
+                  className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  placeholder="********"
+                  name="password"
+                  type="password"
+                  id="pwd"
+                  onChange={handleChange}
+                  autoComplete="password"
+                  className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+
+              {/* <div className="text-sm">
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                  Forgot your password?
+                </a>
+              </div> */}
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Sign in
+              </button>
+            </div>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              New here?{" "}
+              <Link
+                to="/signup"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                click here to get started!
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+
+      {/* <div classNameName="container my-1">
       <Link to="/signup">← Go to Signup</Link>
 
       <h2>Login</h2>
@@ -129,135 +287,8 @@ function LoginVolunteer(props) {
         </div>
       </form>
       <Link to="/LoginCharity">← Charity Login</Link>
-    </div>
-
-
-
-    //Grace's work in progress below: 
-    // <section className="h-screen">
-    //   <div className="container h-full px-6 py-12">
-    //     <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
-    //       <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
-    //         <img
-    //           src={process.env.PUBLIC_URL + "/assets/landing.png"}
-    //           alt="Example"
-    //         />
-    //       </div>
-    //       <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
-    //         <form>
-    //           {/* <!-- Email input --> */}
-    //           <div className="relative mb-6" data-te-input-wrapper-init>
-    //             <input
-    //               type="email"
-    //               className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-    //               id="exampleFormControlInput3"
-    //               placeholder="Email address"
-    //             />
-    //             <label
-    //               for="exampleFormControlInput3"
-    //               className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-    //             >
-    //               Email address
-    //             </label>
-    //           </div>
-    //           {/* 
-    //       <!-- Password input --> */}
-    //           <div className="relative mb-6" data-te-input-wrapper-init>
-    //             <input
-    //               type="password"
-    //               className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-    //               id="exampleFormControlInput33"
-    //               placeholder="Password"
-    //             />
-    //             <label
-    //               for="exampleFormControlInput33"
-    //               className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-    //             >
-    //               Password
-    //             </label>
-    //           </div>
-
-    //           <div className="mb-6 flex items-center justify-between">
-    //             <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
-    //               <input
-    //                 className="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 dark:border-neutral-600 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary dark:checked:border-primary checked:bg-primary dark:checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent"
-    //                 type="checkbox"
-    //                 value=""
-    //                 id="exampleCheck3"
-    //                 checked
-    //               />
-    //               <label
-    //                 classNameName="inline-block pl-[0.15rem] hover:cursor-pointer"
-    //                 for="exampleCheck3"
-    //               >
-    //                 Remember me
-    //               </label>
-    //             </div>
-    //             <a
-    //               href="#!"
-    //               className="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
-    //             >
-    //               Terms and conditions
-    //             </a>
-    //           </div>
-
-    //           {/* <!-- Submit button --> */}
-    //           <button
-    //             type="submit"
-    //             className="inline-block w-full rounded bg-primary px-7 pt-3 pb-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-    //             data-te-ripple-init
-    //             data-te-ripple-color="light"
-    //           >
-    //             Sign up
-    //           </button>
-
-    //           <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-300 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-300">
-    //             <p className="mx-4 mb-0 text-center font-semibold dark:text-neutral-200">
-    //               OR
-    //             </p>
-    //           </div>
-
-    //           <a
-    //             className="mb-3 flex w-full items-center justify-center rounded bg-primary px-7 pt-3 pb-2.5 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
-    //             href="#!"
-    //             role="button"
-    //             data-te-ripple-init
-    //             data-te-ripple-color="light"
-    //           >
-    //             {/* <!-- Facebook --> */}
-    //             <svg
-    //               xmlns="http://www.w3.org/2000/svg"
-    //               className="mr-2 h-3.5 w-3.5"
-    //               fill="currentColor"
-    //               viewBox="0 0 24 24"
-    //             >
-    //               <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-    //             </svg>
-    //             Continue with Facebook
-    //           </a>
-    //           <a
-    //             className="mb-3 flex w-full items-center justify-center rounded bg-info px-7 pt-3 pb-2.5 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)]"
-    //             href="#!"
-    //             role="button"
-    //             data-te-ripple-init
-    //             data-te-ripple-color="light"
-    //           >
-    //             {/* <!-- Twitter --> */}
-    //             <svg
-    //               xmlns="http://www.w3.org/2000/svg"
-    //               className="mr-2 h-3.5 w-3.5"
-    //               fill="currentColor"
-    //               viewBox="0 0 24 24"
-    //             >
-    //               <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-    //             </svg>
-    //             Continue with Twitter
-    //           </a>
-    //         </form>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
+    </div> */}
+    </>
   );
 }
 
