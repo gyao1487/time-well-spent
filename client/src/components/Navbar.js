352511c7a48521662fb474749c38b0f5f380a401
Grace <112779767+grachimus@users.noreply.github.com>
2023-03-20 13:16:50 -0500

    changed logout function to appear in hamburger instead

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -93,7 +93,7 @@ if(isCharity)
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <CharityHamburger />
+                <UserHamburger/> 
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
@@ -160,9 +160,7 @@ if(isCharity)
               </div>
               {Auth.loggedIn() && 
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">               
-                  
                   <Toggle />
-
                   <Menu as="div" className="relative ml-3">
                   <div 
                     className="flex"
@@ -346,6 +344,7 @@ if(isCharity)
                   </div>
                 </div>
               </div>
+              <Toggle />
               {Auth.loggedIn() && 
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                   
