c6d1947e2dea1c236659cb7a503b7d469fb9ba32
KungFuTurtle <m.rfischer5235@gmail.com>
2023-03-20 15:11:59 -0500

    fixed error with edit charity profile

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -266,7 +266,7 @@ const resolvers = {
             new: true,
           }
         );
-
+console.log(userc)
         if (!userc) throw new Error("User not found.");
         return { userc};
       } catch (err) {
