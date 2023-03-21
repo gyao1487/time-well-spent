7b77e309a47c873f56b831ddab7dbeb53d3d3220
Grace <112779767+grachimus@users.noreply.github.com>
2023-03-20 22:47:02 -0500

    bugfix for Volunteer SIgnup - skills field now captures skills

--- client/src/pages/VolunteerSignup.js
+++ client/src/pages/VolunteerSignup.js
@@ -228,15 +228,15 @@ const VolunteerSignup = () => {
 
                 <div>
                   <label
-                    for="fullName"
+                    for="skills"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                   Skills: 
                   </label>
                   <input
                     type="textinput"
-                    name="fullName"
-                    id="fullName"
+                    name="skills"
+                    id="skills"
                     onChange={handleChange}
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Organizing, networking, fundrainsing, etc..."
