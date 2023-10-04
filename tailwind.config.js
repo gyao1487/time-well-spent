908c11c32c317ee1620740c61e9087e58948c0f0
gyao1487 <gee.yao@gmail.com>
2023-10-03 20:49:23 -0500

    edited filepath in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@ const defaultTheme = require("./node_modules/@tailwindcss/forms");
 
 module.exports = {
   content: [
-    "./src/pages/**/*.{html,js,jsx,ts,tsx}",
-    "./src/components/**/*.{html,js,jsx,ts,tsx}",
+    "./src/pages/*.{html,js,jsx,ts,tsx}",
+    "./src/components/*.{html,js,jsx,ts,tsx}",
     "./public/*.html",
     "./node_modules/flowbite/**/*.js",
     "./node_modules/daisyui/**/*.js",
     "./node_modules/@tailwindcss/forms/**/*.js"
   ], 
   purge: [
-    "./src/pages/**/*.{html,js,jsx,ts,tsx}",
-    "./src/components/**/*.{html,js,jsx,ts,tsx}",
+    "./src/pages/*.{html,js,jsx,ts,tsx}",
+    "./src/components/*.{html,js,jsx,ts,tsx}",
     "./public/*.html",
     "./node_modules/flowbite/**/*.js",
     "./node_modules/daisyui/**/*.js",
