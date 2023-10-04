ccc28a0db984e9d490bf5fb02b7568af3957a4d9
gyao1487 <gee.yao@gmail.com>
2023-10-03 20:44:00 -0500

    added purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
     "./node_modules/daisyui/**/*.js",
     "./node_modules/@tailwindcss/forms/**/*.js"
   ], 
+  purge: [
+    "./src/pages/**/*.{html,js,jsx,ts,tsx}",
+    "./src/components/**/*.{html,js,jsx,ts,tsx}",
+    "./public/*.html",
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/daisyui/**/*.js",
+    "./node_modules/@tailwindcss/forms/**/*.js"
+  ],
   darkMode: 'class',
   theme: { 
     extend: {  
